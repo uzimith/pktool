724ac0560f1ab59222140ba08f1eea99c1e28e8d
uzimith <uzimith.x9@gmail.com>
2016-01-04 17:11:36 +0900

    fix ActiveRecord sqlite path

--- lib/models/database.rb
+++ lib/models/database.rb
@@ -3,6 +3,6 @@ require 'active_record'
 module Pktool
   ActiveRecord::Base.establish_connection(
     :adapter => 'sqlite3',
-    :database => 'data/pokemon.sqlite3'
+    :database => __dir__ + '/../../data/pokemon.sqlite3'
   )
 end
--- lib/version.rb
+++ lib/version.rb
@@ -1,3 +1,3 @@
 module Pktool
-  VERSION = "0.2.2"
+  VERSION = "0.2.3"
 end
